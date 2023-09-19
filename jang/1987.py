130f72333790985713f0ec6f6a658c79a7bd3559
JM <jjm6604@gmail.com>
2023-09-19 11:48:23 +0900

    0919

--- jang/1987.py
+++ jang/1987.py
@@ -7,12 +7,18 @@ def backtrack(x, y, cnt):
     global result
     if result < cnt:
         result = cnt
+
+    # 알파벳 26개 다 돌면 더이상 조사 X
     if result == 26:
         print(26)
         exit(0)
+
+    # 사방 돌면서 갈 수 있는지 없는지 조사
     for i in range(4):
         dx = x + direct[i][0]
         dy = y + direct[i][1]
+
+        # 중복되지 않는 경우
         if 0 <= dx < R and 0 <= dy < C and MAP[dx][dy] not in char:
             char.add(MAP[dx][dy])
             backtrack(dx, dy, cnt + 1)
